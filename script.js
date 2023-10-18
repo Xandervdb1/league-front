e4bd60290b3ecc8c6892059b517d7668e70ea509
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-10-18 22:51:46 +0200

    fix

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ const setTeamsContent = (pData, mustWeDraw) => {
     input.setAttribute("placeholder", "Search for a team...");
     var searchIcon = document.createElement("span");
     searchIcon.classList.add("search-icon");
-    searchIcon.innerHTML = '<svg stroke="white" fill="white" stroke-width="0" viewBox="0 0 24 24" height="30px" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"></path></svg>'
+    searchIcon.innerHTML = '<svg stroke="white" fill="white" stroke-width="0" viewBox="0 0 24 24" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg"><path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"></path></svg>'
     inputContainer.append(input, searchIcon);
 
     var datalist = document.createElement("datalist");
@@ -222,7 +222,7 @@ const showSearchResult = (pData) => {
         teamTitleText.setAttribute("data-teamid", team.team_id);
         if (window.innerWidth > 400) {
             if (team.league !== "UNKNOWN") {
-                teamTitleText.innerHTML += "<span class='text-xs'> " + team.league + "</span>"
+                teamTitleText.innerHTML = team.team_name + "<span class='text-xs'> " + team.league + "</span>"
             }
         } else {
             teamTitleText.innerHTML = team.team_name;
